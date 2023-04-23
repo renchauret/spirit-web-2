bc0abecce76a43a9d1d000831174ad65c45cd7d2
Ren Chauret <renaudchauret@gmail.com>
2023-04-23 16:07:03 -0400

    sign in form

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
-    kotlin("js") version "1.7.10"
-    kotlin("plugin.serialization") version "1.7.10"
+    kotlin("js") version "1.8.20"
+    kotlin("plugin.serialization") version "1.8.20"
 }
 
-group = "org.example"
+group = "com.chauret"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -11,17 +11,32 @@ repositories {
 }
 
 kotlin {
-    js {
-        browser {
-            commonWebpackConfig {
-                cssSupport.enabled = true
-            }
-        }
-        binaries.executable()
-    }
+    js(IR) {
+        browser()
+    }.binaries.executable()
+//    js {
+//        browser {
+//            commonWebpackConfig {
+////                cssSupport.enabled = true
+//            }
+//        }
+//        binaries.executable()
+//    }
 }
 
 dependencies {
+//    // tailwind
+//    implementation(npm("tailwindcss", "3.2.1"))
+//    implementation(npm("@tailwindcss/forms", "0.5.3"))
+//
+//    // webpack
+//    implementation(devNpm("postcss", "8.4.17"))
+//    implementation(devNpm("postcss-loader", "7.0.1"))
+//    implementation(devNpm("autoprefixer", "10.4.12"))
+//    implementation(devNpm("css-loader", "6.7.1"))
+//    implementation(devNpm("style-loader", "3.3.1"))
+//    implementation(devNpm("cssnano", "5.1.13"))
+
     //React, React DOM + Wrappers (chapter 3)
     implementation(enforcedPlatform("org.jetbrains.kotlin-wrappers:kotlin-wrappers-bom:1.0.0-pre.354"))
     implementation("org.jetbrains.kotlin-wrappers:kotlin-react")
@@ -38,7 +53,7 @@ dependencies {
 
     //Coroutines & serialization (chapter 8)
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.3")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
 }
 
 // Heroku Deployment (chapter 9)
